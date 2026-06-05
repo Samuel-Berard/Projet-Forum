539b9acad1f28da13eb2011c24c23f9fd4e46fa0
clement <clement.belmondo@outlook.fr>
2026-06-05 12:19:38 +0200

    feat/debut back

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,16 @@
 package api
+
+import (
+	"net/http"
+)
+
+func main() {
+	app := appInitApp()
+	defer app.Close()
+
+	logPrintf("Serveur lancé sur le port %d", app.Config.Port)
+	serverErr := http.ListenAndServe(":8080", app.Router)
+	if serverErr != nil {
+		logFatal("Erreur lors du démarrage du serveur: %v", serverErr)
+	}
+}
