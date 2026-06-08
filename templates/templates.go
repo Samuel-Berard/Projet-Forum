88948340c3d1a2b0071db19a0486743adc9b2230
Sam <samuel.berard@ynov.com>
2026-06-08 12:26:41 +0200

    feat : accueil.html + debut css

--- templates/templates.go
+++ templates/templates.go
@@ -1,1 +1,34 @@
 package templates
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+var ListTemp *template.Template
+
+func RenderTemplate(w http.ResponseWriter, r *http.Request, name string, data interface{}) {
+
+	var buffer bytes.Buffer
+
+	errRender := ListTemp.ExecuteTemplate(&buffer, name, data)
+	if errRender != nil {
+		log.Printf("Template error '%s': %v", name, errRender)
+		http.Redirect(
+			w,
+			r,
+			fmt.Sprintf(
+				"/error?code=%d&message=%s",
+				http.StatusInternalServerError,
+				url.QueryEscape("Error loading the page"),
+			),
+			http.StatusSeeOther,
+		)
+		return
+	}
+	_, _ = buffer.WriteTo(w)
+}
